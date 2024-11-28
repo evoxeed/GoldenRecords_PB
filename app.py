409316b1f39310c8411c63ef61ab7a475fc57dbe
evoxed <nikita.gomozov@gmail.com>
2024-11-28 15:39:58 +0400

    добавил валидацию

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from modules.validation import valid_cells
-
+import time
 # phoneFrame = pd.read_csv('dirty.csv', usecols=['contact_phone'])
 # phoneFrame.to_csv('phone_output.txt', sep='\t', index=False)
 
 dataasd = ['', 'vfbf', '3425', '5566']
-
+start_time = time.time()
 print(valid_cells(dataasd))
+end_time = time.time()
+print(f"Время выполнения программы: {end_time - start_time:.4f} секунд")
--- modules/validation.py
+++ modules/validation.py
@@ -4,12 +4,10 @@ import phonenumbers
 from phonenumbers import carrier
 from phonenumbers.phonenumberutil import number_type
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
 
 def is_non_empty(input_value):
-    # Проверяем, не является ли строка пустой
-    if str(input_value).strip() == '':  # strip() удаляет пробелы в начале и в конце
-        return False
-    return True
+    return bool(input_value.strip())
 
 def is_number(s):
     try:
@@ -19,7 +17,6 @@ def is_number(s):
         return False
 
 def validate_non_empty(validation_function, input_value):
-    # Проверяем, не является ли строка пустой
     if not is_non_empty(input_value):  # strip() удаляет пробелы в начале и в конце
         return False
     else:
@@ -29,48 +26,31 @@ def validate_non_empty(validation_function, input_value):
 def client_bplace_validation(client_bplace): #Можно откоректировать
     return True
 
-def date_validation(client_bday):
-    # Регулярное выражение для проверки формата даты в формате 'YYYY-MM-DD'
-    date_regex = r'^\d{4}-\d{2}-\d{2}$'
-
-    # Проверка на соответствие регулярному выражению
-    if re.match(date_regex, client_bday):
-        try:
-            # Попробуем преобразовать строку в объект datetime
-            datetime.strptime(client_bday, '%Y-%m-%d')
-            return True  # Если дата валидна
-        except ValueError:
-            return False  # Если дата не валидна (например, 2024-02-30)
-    else:
-        return False  # Если формат не соответствует
-
-def phone_validation(phoneNumber):
+def date_validation(date_str):
     try:
-        # Пробуем разобрать номер телефона
-        number2Validate = phoneNumber
-        # Проверяем, является ли номер мобильным
-        if carrier._is_mobile(number_type(phonenumbers.parse(number2Validate))):
-            return True  # Номер телефона валиден и мобильный
-        else:
-            return False  # Номер телефона валиден, но не мобильный
-    except phonenumbers.NumberParseException:
-        return False  # Невалидный номер телефона
+        datetime.strptime(date_str, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+def phone_validation(phone_number):
+    try:
+        number = phonenumbers.parse(phone_number)
+        if phonenumbers.is_mobile(number):
+            return True
+        return False
+    except (phonenumbers.NumberParseException, ValueError):
+        return False
 
 def email_validation(email):
-    if isinstance(email, str):  # Проверяем, является ли email строкой
-        # Регулярное выражение для проверки формата электронной почты
-        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        # Проверяем, соответствует ли email регулярному выражению
-        return re.match(email_regex, email) is not None
-    return False  # Если email не строка, возвращаем False
+    if isinstance(email, str) and re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+        return True
+    return False
 
-def zip_validation(zip_code):
-    if isinstance(zip_code, str):  # Проверяем, является ли zip_code строкой
-        # Регулярное выражение для проверки формата почтового индекса
-        zip_regex = r'^\d{6}$'  # Формат: 123456
-        # Проверяем, соответствует ли zip_code формату
-        return re.match(zip_regex, zip_code) is not None
-    return False  # Если zip_code не строка, возвращаем False
+def zip_code_validation(zip_code):
+    if isinstance(zip_code, str) and re.match(r'^\d{6}$', zip_code):
+        return True
+    return False
 
 def addr_street_validation(input_str):
     if is_number(input_str):
@@ -78,7 +58,7 @@ def addr_street_validation(input_str):
     return True
 
 validationFunctions = {
-    'client_id': False,
+    'client_id': is_non_empty,
     'client_first_name': is_non_empty,
     'client_middle_name': is_non_empty,
     'client_last_name': is_non_empty,
@@ -103,7 +83,7 @@ validationFunctions = {
     'contact_phone': phone_validation,
     'addr_region': is_non_empty,
     'addr_country': is_non_empty,
-    'addr_zip': zip_validation,
+    'addr_zip': zip_code_validation,
     'addr_street': addr_street_validation,
     'addr_str': is_non_empty,
     'addr_house': is_non_empty,
@@ -128,8 +108,7 @@ validationFunctions = {
 }
 
 def valid_cells(data):
-    valid_cells_array = []
-    for index, column in enumerate(data, start=1):
-        if validationFunctions[list(validationFunctions.keys())[index]](column):
-            valid_cells_array.append(index)
-    return valid_cells_array
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(validationFunctions[list(validationFunctions.keys())[index]], column) for index, column in enumerate(data, start=0)]
+        results = [future.result() for future in futures]
+    return [index for index, result in enumerate(results) if result]
